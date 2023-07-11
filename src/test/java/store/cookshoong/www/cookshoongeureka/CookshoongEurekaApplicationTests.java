f6658815b5ce16fa8d20d0ec7b25d5fad7aa4af3
정제완 <jewan8821@naver.com>
2023-07-11 20:41:49 +0900

    CookShoong Eureka Start

---
+++ src/test/java/store/cookshoong/www/cookshoongeureka/CookshoongEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshoongEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
